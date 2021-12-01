fda6aa6ac1161538e5a0421160b5ad9625dc3dd1
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2021-12-01 10:36:49 +0100

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.withIndex().count { (index, value) -> index > 0 && value.toInt() > input[index - 1].toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.subList(0, input.size - 2)
+            .mapIndexed { index, value -> value.toInt() + input[index+1].toInt() + input[index+2].toInt() }
+            .withIndex()
+            .zipWithNext { first, second -> first.value < second.value }
+            .count { it }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
